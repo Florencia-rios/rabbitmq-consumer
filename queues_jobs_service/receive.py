a377100855946a6638db9b53e0d14e01bea4b6c3
Florencia Rios <florencia.rios@etermax.com>
2024-04-09 14:40:54 -0300

    Arregle unos errpres

--- queues_jobs_service/receive.py
+++ queues_jobs_service/receive.py
@@ -23,7 +23,7 @@ def consume_jobs_service():
 
     # la routing key se establece como una cadena vacía ''. Esto significa que cualquier mensaje enviado al intercambio
     # main_exchange será enrutado directamente a la cola main_queue
-    channel.queue_bind(exchange='jobs_with_task_finished_exchange', queue='jobs', routing_key='')
+    channel.queue_bind(exchange='jobs_with_task_finished_exchange', queue='jobs_with_task_finished', routing_key='')
 
     # TODO Para que envíe un mensaje a la vez, hasta que se confirme y luego le pueda enviar otro
     channel.basic_qos(prefetch_count=1)
